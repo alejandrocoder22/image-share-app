f28825d6b247a4c4209de36be3c73e3baeb3b86c
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-02-05 14:51:53 +0100

    Removed unused promises

--- server/src/services/authServices.ts
+++ server/src/services/authServices.ts
@@ -6,16 +6,16 @@ const createUser: any = (credentials: CredentialsEntry) => {
   const SALT_ROUNDS = 10
 
   bcrypt.hash(credentials.password, SALT_ROUNDS, async function (_err, hash) {
-    return await pool.query('INSERT INTO users (username, password) VALUES($1, $2)', [credentials.username, hash])
+    return pool.query('INSERT INTO users (username, password) VALUES($1, $2)', [credentials.username, hash])
   })
 }
 
 const loginUser: any = async (username: string) => {
-  return await pool.query('SELECT * FROM users WHERE username = $1', [username])
+  return pool.query('SELECT * FROM users WHERE username = $1', [username])
 }
 
 const deleteUser: any = async (id: number) => {
-  return await pool.query('DELETE FROM users WHERE ID = $1', [id])
+  return pool.query('DELETE FROM users WHERE ID = $1', [id])
 }
 
 export { createUser, deleteUser, loginUser }
--- server/src/services/imagesServices.ts
+++ server/src/services/imagesServices.ts
@@ -1,9 +1,9 @@
 import pool from '../database'
 
-const getPersonalImages: any = async (userId: number) => await pool.query('SELECT * FROM images WHERE user_id = $1', [userId])
+const getPersonalImages: any = (userId: number) => pool.query('SELECT * FROM images WHERE user_id = $1', [userId])
 
-const createImage: any = async (fileUrl: string, userId: number) => await pool.query('INSERT INTO images (url, user_id) VALUES($1,$2)', [fileUrl, userId])
+const createImage: any = (fileUrl: string, userId: number) => pool.query('INSERT INTO images (url, user_id) VALUES($1,$2)', [fileUrl, userId])
 
-const deleteImage: any = async (id: number) => await pool.query('DELETE FROM images WHERE image_id = $1', [id])
+const deleteImage: any = (id: number) => pool.query('DELETE FROM images WHERE image_id = $1', [id])
 
 export { getPersonalImages, createImage, deleteImage }
