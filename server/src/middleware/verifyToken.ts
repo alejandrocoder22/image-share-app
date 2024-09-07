8f41650f25855bfb61e4ba4855901a4da0ed3951
alejandrocoder22 <alejandro2210lp@gmail.com>
2024-09-07 23:56:19 +0200

    Typed token

--- server/src/middleware/verifyToken.ts
+++ server/src/middleware/verifyToken.ts
@@ -1,19 +1,18 @@
 import jwt from 'jsonwebtoken'
 
 export const verifyToken: any = (req: any, res: any, next: any) => {
-  const token = req.headers.token?.split(' ')[1]
+  const token: string = req.headers.token?.split(' ')[1]
 
-  if (!token) {
+  if (token.length === 0) {
     res.status(400).send({ status: 'FAIL', message: 'You must send a valid token' })
   }
-  if (token) {
-    jwt.verify(token, process.env.JWT_PASSWORD as string, (err: any, decoded: any) => {
-      if (err != null) {
-        res.send(err)
-        return
-      }
-      req.user = decoded
-      next()
-    })
-  }
+
+  jwt.verify(token, process.env.JWT_PASSWORD as string, (err: any, decoded: any) => {
+    if (err != null) {
+      res.send(err)
+      return
+    }
+    req.user = decoded
+    next()
+  })
 }
