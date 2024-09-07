2503606062ad80112dec41b5c4323bf665fe746b
alejandrocoder22 <alejandro2210lp@gmail.com>
2024-09-07 23:50:36 +0200

    Moved interfaces and types to a new folder

---
+++ server/src/interfaces/types.d.ts
@@ -0,0 +1,37 @@
+
+import express from 'express'
+import multer from 'multer'
+export type Visibility = 'public' | 'private'
+
+interface User {
+  user_id?: string
+  username?: string
+  password?: string
+  created_at?: string
+  updated_at?: string
+}
+
+export interface Credentials {
+  password: string
+  username: string
+}
+
+export interface RequestWithUser extends express.Request {
+  user: User
+}
+
+export interface RequestWithUserAndFile extends express.Request {
+  user: User
+  file: UploadedFile
+}
+
+export interface UploadedFile {
+  fieldname: 'string'
+  originalName: 'string'
+  encoding: 'string'
+  mimetype: 'string'
+  destination: 'string'
+  filename: 'string'
+  path: 'string'
+  size: number
+}
