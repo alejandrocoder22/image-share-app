59592b279a84766019847a771a0c0c1830f8fe47
alejandrocoder22 <alejandro2210lp@gmail.com>
2024-09-07 23:56:13 +0200

    Added tryCath on createUser

--- server/src/services/authServices.ts
+++ server/src/services/authServices.ts
@@ -1,21 +1,25 @@
 import pool from '../database'
 import bcrypt from 'bcrypt'
-import { Credentials } from '../types'
+import { Credentials } from '../interfaces/types'
 
 const createUser: any = (credentials: Credentials) => {
   const SALT_ROUNDS = 10
 
-  bcrypt.hash(credentials.password, SALT_ROUNDS, async function (_err, hash) {
-    return pool.query('INSERT INTO users (username, password) VALUES($1, $2)', [credentials.username, hash])
-  })
+  try {
+    bcrypt.hash(credentials.password, SALT_ROUNDS, async function (_err, hash) {
+      return await pool.query('INSERT INTO users (username, password) VALUES($1, $2)', [credentials.username, hash])
+    })
+  } catch (error) {
+    throw new Error('Something went wrong')
+  }
 }
 
 const loginUser: any = async (username: string) => {
-  return pool.query('SELECT * FROM users WHERE username = $1', [username])
+  return await pool.query('SELECT * FROM users WHERE username = $1', [username])
 }
 
 const deleteUser: any = async (id: number) => {
-  return pool.query('DELETE FROM users WHERE ID = $1', [id])
+  return await pool.query('DELETE FROM users WHERE ID = $1', [id])
 }
 
 export { createUser, deleteUser, loginUser }
