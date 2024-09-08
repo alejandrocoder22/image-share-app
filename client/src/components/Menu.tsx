6890434ddedd76944bcb0f62916146f1899b59ef
alejandrocoder22 <alejandro2210lp@gmail.com>
2024-09-08 14:37:24 +0200

    Typed menu correctly

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -3,13 +3,26 @@ import ToggleThemeMode from './ToggleThemeMode'
 import { useContext } from 'react'
 import { UserContext } from '../context/AuthContextProvider'
 
-const Menu: any = (props: any) => {
+interface Props {
+  id: string | undefined
+  setThemeMode: (value: (prev: string) => string) => void
+}
+
+interface UserContextType {
+  user: {
+    isLogged: boolean
+  }
+  setUser: (user: { isLogged: boolean }) => void
+}
+
+const Menu = (props: Props): JSX.Element => {
   const onTogleThemeMode: any = () => {
     props.setThemeMode((prev: string) => prev === 'light' ? 'dark' : 'light')
   }
-  const userContext: any = useContext(UserContext)
+  const userContext = useContext<UserContextType>(UserContext)
+
   const navigate = useNavigate()
-  const onLogout: any = () => {
+  const onLogout = (): void => {
     localStorage.removeItem('token')
     navigate('/login')
     userContext.setUser({
